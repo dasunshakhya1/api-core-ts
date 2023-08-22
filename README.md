e2ce513c914ca47354884b7db5686d0e200d0687
dasunshakhya1 <shakhyajith@gmail.com>
2023-08-22 14:57:20 +0530

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # api-core-ts
+**api-core-ts** API test automation framework written in Typescript.
+
+## Table of Contents
+
+- [Project Overview](#project-overview)
+- [Features](#features)
+
+
+## Project Overview
+
+**api-core** is a simple library that helps test REST APIs.
+
+## Features
+
+- Send GET,POST,DELETE, and PUT requests.
+
+- Use [Mocha](https://www.npmjs.com/package/mocha), [Axios](https://www.npmjs.com/package/axios), and [Typescript](https://www.npmjs.com/package/typescript).
--- test/test-store.ts
+++ test/test-store.ts
@@ -1,10 +1,18 @@
 import {PetController} from "../src/controllers/pet-controller";
 import {expect} from "chai";
 
-describe("asdfa", () => {
-    it('should return all ptes', async  () => {
-const {pets,status} = await PetController.getPetsByStatus("available")
-        console.log(pets[0])
+describe("Test /pet endpoint", () => {
+    it('should return all available pets', async () => {
+        const {pets, status} = await PetController.getPetsByStatus("available")
+        expect(status).to.equal(200)
+    });
+    it('should return all pending pets', async () => {
+        const {pets, status} = await PetController.getPetsByStatus("pending")
+        expect(status).to.equal(200)
+    });
+
+    it('should return all sold pets', async () => {
+        const {pets, status} = await PetController.getPetsByStatus("sold")
         expect(status).to.equal(200)
     });
 })
